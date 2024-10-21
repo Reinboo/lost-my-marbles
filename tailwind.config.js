d15d57b3e9afbd7df03a2abc18fd40fdc9db48bb
Reinboo <nivalis@protonmail.com>
2024-10-21 15:31:02 +0200

    Implement app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
-    content: [],
+    content: [
+		'./components/**/*.{ts,tsx}',
+	],
   theme: {
   	extend: {
   		borderRadius: {
